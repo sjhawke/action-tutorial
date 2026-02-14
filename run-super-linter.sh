eeaa1f91a9a160a7f14f87f3db4555202711ea2d
Simon Hawke <2011967+sjhawke@users.noreply.github.com>
2026-02-14 10:44:40 +0000

    tidy

--- run-super-linter.sh
+++ run-super-linter.sh
@@ -5,6 +5,6 @@ docker run -e LOG_LEVEL=WARN \
         -e FILTER_REGEX_EXCLUDE='^(docs|.github|tests|dist)/' \
         -e VALIDATE_ALL_CODEBASE=true  \
         -e LOG_FILE=/tmp/lint/super-linter.log \
-        -v $(pwd):/tmp/lint \
+        -v '$(pwd)':/tmp/lint \
         -e DEFAULT_BRANCH=main \
         ghcr.io/super-linter/super-linter:latest
